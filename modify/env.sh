75394e444be3b97db90aa4fd59961b2773a23aa4
MBRjun <mbrjun@foxmail.com>
2022-11-25 18:42:06 +0800

    Add modify-scripts

---
+++ modify/env.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+sudo apt install nodejs npm curl
+#to do: switch to yarn
+wget -O ./nodejs.tar.xz https://nodejs.org/dist/v19.1.0/node-v19.1.0-linux-x64.tar.xz
+tar -xvf ./nodejs.tar.xz
+cd node-v19.1.0-linux-x64
+sudo cp -R ./* /usr
+#thats fuck-apt command
+sudo npm install -g npm@latest
+#GOODCOMMAND
+cp modify/theme.yml themes/tranquilpeak/_config.yml
+cd themes/tranquilpeak
+npm install && npm run prod
+#Install theme
+echo Finish.
